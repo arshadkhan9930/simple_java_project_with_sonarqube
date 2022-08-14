4a11d7af80ffd102e599604332c797f9fe97a7fa
Arshad Khan <33569877+arshadkhan9930@users.noreply.github.com>
2022-08-14 18:12:35 +0530

    Update Calculator.java

--- src/main/java/my/simplejavaprojectwithsonarqube/Calculator.java
+++ src/main/java/my/simplejavaprojectwithsonarqube/Calculator.java
@@ -24,10 +24,4 @@ public class Calculator {
 		return a*b;
 	}
 	
-		public int getRandomNumber() {
-	System.out.println("vulnerability on purpose");
-	SecureRandom sr = new SecureRandom();
-	sr.setSeed(123456L);
-	return sr.nextInt();
-	}		
 }
